3d804cf39f1393348dfcf08606628e9503485772
Alan Jones <alanjones83@gmail.com>
2017-08-29 17:26:56 -0500

    fixed route to redirect to hero component

--- Teams project/src/app/app-routing.module.ts
+++ Teams project/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-/* import { DashboardComponent }   from './dashboard.component'; */
 import { HeroesComponent }      from './heroes.component';
-/* import { HeroDetailComponent }  from './hero-detail.component'; */
+
 
 const routes: Routes = [
- /*  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, */
-  /* { path: 'dashboard',  component: DashboardComponent }, */
-/*   { path: 'detail/:id', component: HeroDetailComponent }, */
+  {path:'',redirectTo:'heroes', pathMatch: 'full' },
   { path: 'heroes',     component: HeroesComponent }
 ];
 
