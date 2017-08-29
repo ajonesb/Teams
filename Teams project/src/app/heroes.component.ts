159975a89e194d4ffb22e6f8f8310944be57a877
Alan Jones <alanjones83@gmail.com>
2017-08-29 18:00:17 -0500

    Update heroes.component.ts

--- Teams project/src/app/heroes.component.ts
+++ Teams project/src/app/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
-import { Hero }              from './hero';
-import { HeroService }       from './hero.service';
+
+import { Hero }                from './hero';
+import { HeroService }         from './hero.service';
 
 @Component({
   selector: 'my-heroes',
@@ -34,7 +35,7 @@ export class HeroesComponent implements OnInit {
 
   delete(hero: Hero): void {
     this.heroService
-        .delete(hero.id)
+        .delete(hero.numero)
         .then(() => {
           this.heroes = this.heroes.filter(h => h !== hero);
           if (this.selectedHero === hero) { this.selectedHero = null; }
