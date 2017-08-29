b420ca53cf3cbd19bbb54fa2197de3efaf0b80b0
Alan Jones <alanjones83@gmail.com>
2017-08-29 17:57:18 -0500

    changed id to corresponding numero for team object

--- Teams project/src/app/in-memory-data.service.ts
+++ Teams project/src/app/in-memory-data.service.ts
@@ -2,35 +2,35 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 0,  name: 'Zero' },
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { numero: 0,  name: 'Zero' },
+      { numero: 11, name: 'Mr. Nice' },
+      { numero: 12, name: 'Narco' },
+      { numero: 13, name: 'Bombasto' },
+      { numero: 14, name: 'Celeritas' },
+      { numero: 15, name: 'Magneta' },
+      { numero: 16, name: 'RubberMan' },
+      { numero: 17, name: 'Dynama' },
+      { numero: 18, name: 'Dr IQ' },
+      { numero: 19, name: 'Magma' },
+      { numero: 20, name: 'Tornado' }
     ];
     return {heroes};
   }
 
   createDbTwo() {
-    const heroesTwo = [
-      { id: 0,  name: 'Alan' },
-      { id: 11, name: 'Bob' },
-      { id: 12, name: 'Carlos' },
-      { id: 13, name: 'Alex' },
-      { id: 14, name: 'Sam' },
-      { id: 15, name: 'Mike' },
-      { id: 16, name: 'Brian' },
-      { id: 17, name: 'David' },
-      { id: 18, name: 'Drake' },
-      { id: 19, name: 'Mario' },
-      { id: 20, name: 'Tom' }
+    const heroes = [
+      { numero: 0,  name: 'Alan' },
+      { numero: 11, name: 'Bob' },
+      { numero: 12, name: 'Carlos' },
+      { numero: 13, name: 'Alex' },
+      { numero: 14, name: 'Sam' },
+      { numero: 15, name: 'Mike' },
+      { numero: 16, name: 'Brian' },
+      { numero: 17, name: 'Davnumero' },
+      { numero: 18, name: 'Drake' },
+      { numero: 19, name: 'Mario' },
+      { numero: 20, name: 'Tom' }
     ];
-    return {heroesTwo};
+    return {heroes};
   }
 }
