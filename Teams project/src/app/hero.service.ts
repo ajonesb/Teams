e2a9f5ebccc79b6d4c0d4857e673d6a97e584b5a
Alan Jones <alanjones83@gmail.com>
2017-09-01 10:45:47 -0500

    Update hero.service.ts

--- Teams project/src/app/hero.service.ts
+++ Teams project/src/app/hero.service.ts
@@ -15,10 +15,10 @@ export class HeroService {
   // This function here gets heroes from in-memory-data.service.ts to show them on Team 1 
   getHeroes(): Promise<Hero[]> { //get returns an observable 
     return this.http.get(this.heroesUrl)
-               .toPromise()//converted to promise, imported from rxjs
-               .then(response => response.json().data as Hero[])//convert to js object, heroe array
-               .catch(this.handleError); 
-               /*At the end of getHeroes(), you catch server failures and pass them to an error handler. */ 
+      .toPromise()//converted to promise, imported from rxjs
+      .then(response => response.json().data as Hero[])//convert to js object, heroe array
+      .catch(this.handleError); 
+      /*At the end of getHeroes(), you catch server failures and pass them to an error handler. */ 
   }
 
   //delete function to delete heroes for teams list on Team 1
@@ -30,24 +30,17 @@ export class HeroService {
       .catch(this.handleError);
   }
 
-  // this function here created heroe to add to list on Team 1
-  create(name: string): Promise<Hero> {
-    return this.http
-      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
-      .toPromise()
-      .then(res => res.json().data as Hero)
+  // this function here creates the new heroe to add to list on Team 1
+  create(name: string): Promise<Hero> {//creates new heroe
+//return the heroe with heroe url, stringify object we get, since not whole object..
+//create object and tell it it's a name, add http headers to allow client and server to pass additional info.
+    return this.http.post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()//will be observable then converte to promise
+      .then(res => res.json().data as Hero)//return response back to add to array
       .catch(this.handleError);
   }
 
- /*  update(hero: Hero): Promise<Hero> {
-    const url = `${this.heroesUrl}/${hero.numero}`;
-    return this.http
-      .put(url, JSON.stringify(hero), {headers: this.headers})
-      .toPromise()
-      .then(() => hero)
-      .catch(this.handleError);
-  } */
-
+  //error handing
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
